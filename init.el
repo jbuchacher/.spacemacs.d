a0deab70bc08a284926f3dcdd136a06b5cfc943e
Joshua Buchacher <jbuchacher@me.com>
2016-03-21 08:46:48 -0700

    Add darcula-theme

--- init.el
+++ init.el
@@ -100,11 +100,7 @@ values."
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(spacemacs-dark
                          spacemacs-light
-                         solarized-light
-                         solarized-dark
-                         leuven
-                         monokai
-                         zenburn)
+                         darcula)
    ;; If non nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font. `powerline-scale' allows to quickly tweak the mode-line
